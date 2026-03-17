44e8f4f9215ace80ea02ad0fa8ae3994285393c0
IanR333 <labsfiap@fiap.com.br>
2026-03-16 22:41:36 -0300

    AULA 2 python variaveis

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,39 @@
+from pyparsing import stringStart
+
+print("to tacando na xuxa")
+
+print(7+4)
+
+print("7" + "4")  # concatena stringStart
+
+#comentarios de 1 linha no python
+
+# VARIAVES
+
+nome = ("alexandre") #str
+
+print(nome)
+idade=26 #int
+peso= 70.2 # float
+
+print(nome,idade,peso)
+
+#INPUT SIMULAR FORMULARIO NO CMD
+
+nome= input("DIGITE O SEU NOME:") # string
+
+idade = int(input("Digite sua idade:"))
+Peso = float(input("digite seu peso :"))
+
+
+print (nome,idade,peso)
+
+print(f"oiii,{nome}")
+
+
+
+
+
+
+
+
---
+++ aula02-variaveis-operadores/desafio ne.py
@@ -0,0 +1,29 @@
+
+
+
+nome= input("DIGITE O SEU NOME:")
+
+
+
+
+
+if nome == "claudio":
+    print("boas vindas claudio")
+
+
+
+
+
+
+
+
+nome = input("DIGITE O SEU NOME:")
+
+if nome == "flavia":
+    print("boas vindas flavia")
+
+
+
+
+
+
